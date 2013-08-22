840c35d3b029114c336a6e7ffeddcdb855896187
Brandon Savage <brandon@brandonsavage.net>
2013-08-22 10:34:20 -0700

    Fixes Bug 855560 - Adding the Mozilla MPL license header to all Mozilla-created source files.

--- cache/BugzillaCacheDummy.class.php
+++ cache/BugzillaCacheDummy.class.php
@@ -1,5 +1,7 @@
 <?php
-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 /**
  * Dummy cache for those times you want to test the functionality WITHOUT the
  * cache.
