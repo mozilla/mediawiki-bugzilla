6e198f98ecdfd350e840ad9c2b2df6faa45ac0e9
Ralf Habacker <ralf.habacker@sag.eu>
2015-09-07 11:12:43 +0200

    Fix postgresql syntax

--- cache/sql/postgresql.sql
+++ cache/sql/postgresql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE bugzilla_cache (
-    `id`          SERIAL PRIMARY KEY,
-    `key`         VARCHAR(255) UNIQUE NOT NULL DEFAULT '',
-    `data`        TEXT,
-    `expires`     INTEGER NOT NULL DEFAULT 0
+    id          SERIAL PRIMARY KEY,
+    key         VARCHAR(255) UNIQUE NOT NULL DEFAULT '',
+    data        TEXT,
+    expires     INTEGER NOT NULL DEFAULT 0
 );
