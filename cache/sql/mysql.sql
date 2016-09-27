eaa6d855c30d6435313978a88864413fc74bcd73
Romain d'Alverny <rdalverny@gmail.com>
2016-09-27 11:23:40 +0200

    Use MediaWiki cache strategy and code
    This removes all BugzillaCache* classes and code that used it,
    replaces it with MediaWiki `wfGetCache($wgMainCacheType)`,
    which provides sensibly the same set of options.
    
    Doing so also simplifies a first install, by removing specific
    configuration for this extension.

--- cache/sql/mysql.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS /*$wgDBprefix*/bugzilla_cache (
-  `id` integer(40) NOT NULL AUTO_INCREMENT,
-  `key` varchar(255) NOT NULL DEFAULT '',
-  `data` longtext,
-  `expires` integer(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX /*i*/uniq_bugzilla_cache_key (`key`)
-) /*$wgDBTableOptions*/;
